ab0efec113acab5a0f4625faa3d85421fde9dbd2
Andrew Bethel <andrew@striking.ly>
2015-12-18 16:03:05 -0600

    styling and config refactor

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,15 @@ module.exports = {
   entry: './js/index.js',
   output: {
     path: __dirname + '/dist',
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react', 'stage-0'],
-        },
-      },
-    ],
-  },
+        loader: 'babel'
+      }
+    ]
+  }
 };
