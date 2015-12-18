a2b1e76668a8cd150a8b1f5060834059c82f30ea
vgabruzzo <vgabruzzo@gmail.com>
2015-12-18 17:25:47 -0500

    add webpack production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/app',
+  entry: './js/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-0'],
+        },
+      },
+    ],
+  },
+};
