c3d7cb4537d622e6fdb6d9379f6592ad03be4afe
Mattias Lundberg <mattiaslundberg@openmailbox.org>
2016-09-04 15:55:01 +0200

    Use textarea to handle linebreaks in text.

--- app/js/components/input.js
+++ app/js/components/input.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
 class Input extends React.Component {
-  constructor() {
-    super()
-  }
-
   render() {
     return (
-      <input onKeyUp={this.props.onType} type="text"></input>
+      <textarea onKeyUp={this.props.onType}></textarea>
     )
   }
 }
