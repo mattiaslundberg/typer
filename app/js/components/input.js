b3414cd7df564174b6ee98b3d8ace8bef692a91d
Mattias Lundberg <mattiaslundberg@openmailbox.org>
2016-09-11 17:49:56 +0200

    Automatically focus input when starting.

--- app/js/components/input.js
+++ app/js/components/input.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 class Input extends React.Component {
+  constructor(props) {
+    super(props)
+    this.forceFocus = this.forceFocus.bind(this)
+    this.forceFocus()
+  }
+
+  forceFocus() {
+    if (this._input) {
+      this._input.focus()
+    } else {
+      setTimeout(this.forceFocus, 1)
+    }
+  }
+
   render() {
     return (
-      <textarea onKeyUp={this.props.onType}></textarea>
+      <textarea ref={(c) => this._input = c} onKeyUp={this.props.onType}></textarea>
     )
   }
 }
