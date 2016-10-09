0aed6421bd287840cd1e8cdc40e6148bb9791861
Mattias Lundberg <mattiaslundberg@openmailbox.org>
2016-10-09 20:03:35 +0200

    Add devenv setup script.

---
+++ server/devenv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+export APP_SECRET_KEY="oijsofijwarijw032u40832ujfoaf"
+export OAUTH_REDIRECT_URI="http://localhost:8080/api/oauth2callback"
+# export OAUTH_CLIENT_ID="..."
+# export OAUTH_CLIENT_SECRET="..."
+export EVE_DEBUG="True"
