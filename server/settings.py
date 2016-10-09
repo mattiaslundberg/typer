5af0442230dbc9a8c7316bfe79a4a21b7105d8fd
Mattias Lundberg <mattiaslundberg@openmailbox.org>
2016-10-09 14:28:03 +0200

    Add settings for server.

--- server/settings.py
+++ server/settings.py
@@ -1,3 +1,5 @@
+import os
+
 DOMAIN = {
     "texts": {
         "schema": {
@@ -16,3 +18,13 @@ DOMAIN = {
 
 RESOURCE_METHODS = ["GET", "POST"]
 ITEM_METHODS = ["GET"]
+
+
+APP_SECRET_KEY = os.environ["APP_SECRET_KEY"]
+OAUTH_CLIENT_ID = (os.environ["OAUTH_CLIENT_ID"])
+OAUTH_CLIENT_SECRET = os.environ["OAUTH_CLIENT_SECRET"]
+OAUTH_REDIRECT_URI = os.environ["OAUTH_REDIRECT_URI"]
+OAUTH_AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
+OAUTH_TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+OAUTH_USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
+OAUTH_SCOPE = 'email'
